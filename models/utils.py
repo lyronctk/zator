782c5e3118c3e0e4e7bf1faf2881c5e59008ef84
Varun Shenoy <varun.nshenoy@gmail.com>
2023-01-11 13:46:39 -0800

    padded

--- models/utils.py
+++ models/utils.py
@@ -68,11 +68,35 @@ component main { public [prevHash, in] } = TailLayer();"""
 
 def smoke():
     arr = "["
-    for i in range(32):
+    for i in range(3136):
         arr += "0"
         if i != 3135:
             arr += ","
     arr += "]"
     pyperclip.copy(arr)
 
-smoke()
+def pad_json():
+    with open('json/inp1_two_conv_mnist.json') as f:
+        data = json.load(f)
+
+        head_activ = np.array(data["head"]["a"])
+        backbone = data["backbone"]
+
+        # pad head_active to 6x6x2
+        head_activ = np.pad(head_activ, ((1, 1), (1, 1), (0,0)), 'constant', constant_values=0)
+        polyd = head_activ ** 2 + head_activ
+
+        for idx, bone in enumerate(backbone):
+            bone_activ = np.array(bone["a"])
+            bone_activ = np.pad(bone_activ, ((1, 1), (1, 1), (0,0)), 'constant', constant_values=0)
+            polyd = bone_activ ** 2 + bone_activ
+            backbone[idx]["a"] = polyd.tolist()
+
+        data["head"]["a"] = polyd.tolist()
+        
+        with open('json/PAD_inp1_two_conv_mnist.json', 'w') as outfile:
+            json.dump(data, outfile)
+        
+
+
+pad_json()

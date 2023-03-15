8023d0ef5bb626d44d2379ba92b92d0d6f3baef2
lyronctk <lyronctk@stanford.edu>
2023-03-15 19:08:04 +0000

    smoke test with cpp witness gen works

---
+++ nova/circom/tests/backbone_smoke_cpp.sh
@@ -0,0 +1,21 @@
+# ==
+# Boilerplate circuit compilation for development
+# ==
+
+# Compile circuit
+circom ../Backbone.circom --r1cs --c --prime vesta
+
+# Generate the witness, primarily as a smoke test for the circuit
+make -C Backbone_cpp/
+./Backbone_cpp/Backbone backbone_smoke.json Backbone.wtns
+
+# Create out directory if it doesn't exist
+if [ ! -d "../out" ]; then
+  echo "Directory 'out' does not exist. Creating directory 'out'."
+  mkdir "../out"
+fi
+
+# Clean up
+mv Backbone_cpp/Backbone ../out
+mv Backbone.r1cs ../out
+rm -r Backbone_cpp/ Backbone.wtns
